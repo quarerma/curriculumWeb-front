7ee494bd9cabbfd081d3e393d2aefb0dacd63c90
quarerma <gabriel.oliveira.quaresma@gmail.com>
2023-12-26 20:55:55 -0300

    Header config

---
+++ src/pages/home/components/header/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <div className="h-[15vh] w-screen bg-white">
+      <div className="top-[40%] relative">components</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/header/navbar";
+
+export const Home = () => {
+  return (
+    <div className="bg-cyan-200 h-screen">
+      <Navbar />
+      <div> home</div>
+    </div>
+  );
+};
+
+export default Home;
